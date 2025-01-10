6d036180a83b8beb9dfc602b465adff32aafad08
Gustavo1701 <gustavojr.lg.10@gmail.com>
2025-01-09 21:46:49 -0300

    Atualização

---
+++ db/db.pasteis.js
@@ -0,0 +1,17 @@
+export let pasteis = [
+    {
+        id: 1,
+        nome: 'Excelentissimo',
+        valor: 20.90,
+        tamanho: 'Grande',
+        viagem: true
+
+    },
+    {
+        id: 2,
+        nome: 'My Love',
+        valor: 15.90,
+        tamanho: 'Pequeno',
+        viagem: false
+    }
+];
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //1- importando o fastifay
 import Fastify from "fastify";
+import { pasteis } from "./db/db.pasteis.js";
 
 const fastify = Fastify({
     logger: true
@@ -9,6 +10,41 @@ const fastify = Fastify({
 fastify.get('/', (request, response) => {
     return {message: 'OI! Chamado a partir da raiz!'}
 });
+
+fastify.get('/pasteis', (request, response) => {
+    return {
+        data: pasteis,
+        qtd: pasteis.length,
+        message: 'Retornou todos os pasteis!'
+    }
+});
+
+fastify.post('/pastel', (request, response) => {
+   
+    const pastel = request.body;
+
+    const pastelExistente = pasteis.find(p => p.id === pastel.id);
+
+    if(pastelExistente){
+        return response.status(400).send({
+            message: `O pastel de id: ${pastel.id} já existe`
+        });
+    } 
+
+    pasteis.push(pastel);
+
+    return{
+        messege: 'Pastel adicionado com sucesso!',
+        qtd: pasteis.length,
+        data: pasteis,
+    }
+});
+
+// fastify.get('/pastel/:id', (request, response) => {
+//     const id = parseInt(request.params.id);
+
+//     const produto = 
+// });
 
 //3- Inicializar o servidor
 fastify.listen({port: 3000});
